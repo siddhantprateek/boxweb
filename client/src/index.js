4fc2c189c93f9b7e18bedb3c505986a41080d40f
siddhantprateek <siddhantprateek@gmail.com>
2022-06-22 18:38:16 +0530

    removed ts

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root')
 );
 root.render(
   <React.StrictMode>
