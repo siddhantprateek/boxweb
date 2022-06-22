7997448f58ac0c7189bcf78bd7577069e5d4d29a
siddhantprateek <siddhantprateek@gmail.com>
2022-06-22 17:50:11 +0530

    react router dom setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Home, Discover, Market } from './pages';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='discover' element={<Discover />} />
+        <Route path='marketplace' element={<Market />} />
+      </Routes>
     </div>
   );
 }
