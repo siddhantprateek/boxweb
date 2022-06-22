6f1b5fb319f312efe7e9ca182998311f1e7890a5
siddhantprateek <siddhantprateek@gmail.com>
2022-06-22 17:49:38 +0530

    default export of component and pages

---
+++ client/src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Header } from './header/header.components';
+export { default as Footer } from './footer/footer.components';
+export { default as NftCard } from './nftcard/nftcard.components'; 
---
+++ client/src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Home } from './home/home.pages';
+export { default as Market } from './marketplace/market.pages';
+export { default as Discover } from './discover/discover.pages';
