19a10598643584bc22405e2f404366d565ff5bfe
siddhantprateek <siddhantprateek@gmail.com>
2022-06-23 19:36:52 +0530

    home page head created

--- client/src/pages/home/home.pages.jsx
+++ client/src/pages/home/home.pages.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NftCard } from "../../components";
-import './home.styles.css'
+import "./home.styles.css";
 
 const Home = () => {
   return (
@@ -12,8 +12,24 @@ const Home = () => {
         <div className="oval-4"></div>
         <div className="oval-5"></div>
       </div>
-      Home
-      
+      <div className="home-head-container">
+        <div className="home-content">
+          <h1>
+            Discover <span className="head-bold"> Rare Collections</span> of{" "}
+            <span className="head-bold">
+              Arts & NFTs.<div className="green-highlight"></div>
+            </span>
+          </h1>
+        </div>
+        <div className="tilted-cards">
+          <div className="nft-head-card">
+            <NftCard />
+          </div>
+          <div className="nft-head-card-2">
+          <NftCard />
+        </div>
+        </div>
+      </div>
     </div>
   );
 };
