81157c92ec7c55591a6f3203f7750acb3866705c
siddhantprateek <siddhantprateek@gmail.com>
2022-06-23 17:53:18 +0530

    home page bg

--- client/src/pages/home/home.pages.jsx
+++ client/src/pages/home/home.pages.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { NftCard } from "../../components";
+import './home.styles.css'
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-page">
+      <div className="home-bg">
+        <div className="oval-1"></div>
+        <div className="oval-2"></div>
+        <div className="oval-3"></div>
+        <div className="oval-4"></div>
+        <div className="oval-5"></div>
+      </div>
       Home
-      <NftCard />
+      
     </div>
   );
 };
