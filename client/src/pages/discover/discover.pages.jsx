798a16e481a08d231503375c79b5fe7c50a68982
suvangipaul <suvangipaul8@gmail.com>
2022-06-23 12:32:58 +0530

    Discover Page Created

--- client/src/pages/discover/discover.pages.jsx
+++ client/src/pages/discover/discover.pages.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import "./discover.styles.css";
 import { Explorecard } from "../../components";
 
 const Discover = () => {
   return (
-    <div>
-      <Explorecard />
+    <div className="discover-container">
+      <div className="head-dis">
+        <h1>EXPLORE</h1>
+        <div className="greenbox"></div>
+      </div>
+
+      <div className="header-discover">
+        <ul className="nav-dis">
+          <li>Trending</li>
+          <li>Top</li>
+          <li>Art</li>
+          <li>Collectibles</li>
+          <li>Domain Name</li>
+          <li>Trading</li>
+          <li>Virtual Worlds</li>
+        </ul>
+      </div>
+      <div className="dis-explore">
+        <Explorecard />
+      </div>
     </div>
   );
 };
