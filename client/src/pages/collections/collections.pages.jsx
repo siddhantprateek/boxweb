1bc24d808a437ebd64a10364eab846721c425b60
Siddhant Prateek <siddhantprateek@gmail.com>
2022-07-02 23:36:25 +0530

    nft collection api integrated collection

--- client/src/pages/collections/collections.pages.jsx
+++ client/src/pages/collections/collections.pages.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { ThirdwebSDK } from '@thirdweb-dev/sdk'
+import React, { useEffect } from 'react'
 
 const Collections = () => {
+  useEffect(() => {
+    const sdk = new ThirdwebSDK('rinkeby')
+    const contract = sdk.getMarketplace("0x81012D50531BaE54062bB817258dAcEFa01cc14d")
+    const get_marketplace = async () => {
+      const listings = await contract.getActiveListings();
+      console.log(listings)
+      console.log("price:", listings[0].buyoutCurrencyValuePerToken.displayValue)
+    }
+    get_marketplace()
+
+  }, [])
+
   return (
-    <div>Collections</div>
+    <div>
+      <h1>Collection</h1>
+    </div>
   )
 }
 
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './home/home.pages';
 export { default as Market } from './marketplace/market.pages';
 export { default as Discover } from './discover/discover.pages';
 export { default as Nomatch } from './nomatch/notmatch.pages';
+export { default as Collections } from './collections/collections.pages';
