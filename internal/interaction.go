3d96c378977250fc9f39049268333886dbae4ceb
Siddhant Prateek <siddhantprateek@gmail.com>
2023-01-29 13:22:28 +0530

    go internals

---
+++ internal/interaction.go
@@ -0,0 +1,24 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func ConnectNetwork() {
+	// Ganache RPC Server: HTTP://127.0.0.1:7545
+	// you can provide infura mainnet id
+	conn, err := ethclient.Dial("HTTP://127.0.0.1:7545")
+	if err != nil {
+		fmt.Println("Unable to connect Ethereum client")
+	}
+
+	ctx := context.Background()
+	tx, pending, _ := conn.TransactionByHash(ctx, common.HexToHash(""))
+	if !pending {
+		fmt.Println(tx)
+	}
+}
