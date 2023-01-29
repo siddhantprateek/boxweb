ebff6f9181bc1b44aa13822ba261e891ed785ede
Siddhant Prateek <siddhantprateek@gmail.com>
2023-01-29 13:24:15 +0530

    app configs

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config()
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
