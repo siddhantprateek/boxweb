9e00a0beae788927b3569fcfc8ddc47890940590
Siddhant Prateek <siddhantprateek@gmail.com>
2023-01-29 13:23:25 +0530

    go packages

---
+++ pkg/get_account.go
@@ -0,0 +1,54 @@
+package pkg
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func getAccountAuth(client *ethclient.Client, accountAddress string) *bind.TransactOpts {
+
+	privateKey, err := crypto.HexToECDSA(accountAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		panic("invalid key")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	// fetch the last use nonce of amount
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Nonce: ", nonce)
+
+	chainId, err := client.ChainID(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainId)
+	if err != nil {
+		panic(err)
+	}
+
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = uint64(300000)
+	auth.GasPrice = big.NewInt(1000000)
+
+	return auth
+
+}
